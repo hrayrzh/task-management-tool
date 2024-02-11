bb41f902feca50989e397bb43175b8fcd7000c6e
Hrayr <hr.zohrabyan@gmail.com>
2024-02-12 01:39:48 +0400

    second commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import React, { useState, useEffect } from 'react';
 import Card from './card'
 
 
+function replaceItem(items, newItem) {
+    return items.map(item => {
+        if (item.id === newItem.id) {
+            return newItem;
+        }
+        return item;
+    });
+}
 
 function App() {
     const [data, setdata] =useState([])
-    
+
     useEffect(() => {
         fetch('/data.json')
             .then(response => response.json())
@@ -19,36 +27,74 @@ function App() {
     }, []);
     
     
-    const cons = () => {
-      console.log("a")
+    const taskChange = (task) => {
+      let newData = replaceItem(data, task);
+      setdata(newData);
     }
 
+    useEffect(() => {
+        console.log(data);
+    }, [data])
 
-    console.log(data);
+
     
   return (
     <div className="container">
         <div>
            <h6>Backlog</h6>
-            {data.filter(task => task.status === 'backlog').map((task) => <Card props={task} cons={cons}/>)}
+            {data.filter(task => task.status === "backlog").length > 0 ? (
+                data.filter(task => task.status === "backlog").map((task) => (
+                    <Card
+                        props={task}
+                        taskChange={taskChange}
+                        key={task.id}
+                    />
+                ))
+            ) : (
+                <div className="card emptySection">SECTION IS EMPTY</div>
+            )}
         </div>
         <div>
            <h6>ToDo</h6>
-            {data.map((task) => (
-                task.status === "todo" && <Card props={task} cons={cons}/>
-            ))}
+            {data.filter(task => task.status === "todo").length > 0 ? (
+                data.filter(task => task.status === "todo").map((task) => (
+                    <Card
+                        props={task}
+                        taskChange={taskChange}
+                        key={task.id}
+                    />
+                ))
+            ) : (
+                <div className="card emptySection">SECTION IS EMPTY</div>
+            )}
         </div>
         <div>
            <h6>In Progress</h6>
-            {data.map((task) => (
-                task.status === "in progress" && <Card props={task}/>
-            ))}
+            {data.filter(task => task.status === "in progress").length > 0 ? (
+                data.filter(task => task.status === "in progress").map((task) => (
+                    <Card
+                        props={task}
+                        taskChange={taskChange}
+                        key={task.id}
+                    />
+                ))
+            ) : (
+                <div className="card emptySection">SECTION IS EMPTY</div>
+            )}
         </div>
         <div>
-           <h6>Done</h6>
-            {data.map((task) => (
-                task.status === 'done' && <Card props={task}/>
-            ))}
+            <h6>Done</h6>
+            {data.filter(task => task.status === "done").length > 0 ? (
+                data.filter(task => task.status === "done").map((task) => (
+                    <Card
+                        props={task}
+                        taskChange={taskChange}
+                        key={task.id}
+                    />
+                ))
+            ) : (
+                <div className="card emptySection">SECTION IS EMPTY</div>
+            )}
         </div>
       
       </div>
--- src/card.jsx
+++ src/card.jsx
@@ -2,14 +2,69 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import React, { useState, useEffect } from 'react';
 
-function Card({props, cons}) {
+
+function updateTask(item, key, value) {
+    let newData = item;
+    newData[key] = value;
+    return newData
+}
+
+
+function Card({props, taskChange}) {
 
     const {status, assignee, title, description, priority} = props;
 
     const [editTitle, setEditTitle] = useState(false);
-    const [editAssigne, setEditAssigne] = useState(false);
+    const [editAssignee, setEditAssignee] = useState(false);
     const [editDescription, setEditDescription] = useState(false);
-    console.log(cons);
+    const [assigneeInput, setAssigneeInput] = useState(assignee);
+    const [titleInput, setTitleInput] = useState(title);
+    const [descriptionInput, setDescriptionInput] = useState(description);
+
+
+    const handleStatusChange = (e) => {
+        let updatedTask = updateTask(props, "status", e.target.value);
+        taskChange(updatedTask);
+    }
+
+    const handlePriorityChange = (e) => {
+        let updatedTask = updateTask(props, "priority", e.target.value);
+        taskChange(updatedTask);
+    }
+
+    const assigneeChange = () =>{
+        let updatedTask = updateTask(props, "assignee", assigneeInput);
+        taskChange(updatedTask);
+        setEditAssignee(!editAssignee);
+    }
+
+    const assigneeCancel = () =>{
+        setAssigneeInput(assignee);
+        setEditAssignee(!editAssignee);
+    }
+
+    const titleChange = () =>{
+        let updatedTask = updateTask(props, "title", titleInput);
+        taskChange(updatedTask);
+        setEditTitle(!editTitle);
+    }
+
+    const titleCancel = () =>{
+        setTitleInput(title);
+        setEditTitle(!editTitle);
+    }
+
+    const descriptionChange = () =>{
+        let updatedTask = updateTask(props, "description", descriptionInput);
+        taskChange(updatedTask);
+        setEditDescription(!editDescription);
+    }
+
+    const descriptionCancel = () =>{
+        setDescriptionInput(description);
+        setEditDescription(!editDescription);
+    }
+
 
 
 
@@ -18,36 +73,47 @@ function Card({props, cons}) {
 
   return (
             <div className="card" >
-                <div className={title}>
-                    { editTitle ?  <input value={title} /> : <span>{title}</span>}
 
-                    <button onClick={() => setEditTitle(!editTitle)}>
-                        { !editTitle ? '🖋' :  "📌"}
-                    </button>
-                </div>
-                <div>
-                    { editDescription ? <textarea value={description}/> : <span>{description}</span>}
-                    <button onClick={() => setEditDescription(!editDescription)}>
-                        { !editDescription ? '🖋' :  "📌"}
-                    </button>
-                </div>
-                <select>
-                  <option value="backlog">Backlog</option>
-                  <option value="todo">ToDo</option>
-                  <option value="in progress">In Progress</option>
-                  <option value="done">Done</option>
+                { editTitle ?  <div>
+                    <input value={titleInput} onChange={(e) => setTitleInput(e.target.value)}/>
+                    <button onClick={titleChange}>👍</button>
+                    <button onClick={titleCancel}>👎</button>
+                </div>: <div>
+                    <span>{titleInput}</span>
+                    <button onClick={() => setEditTitle(!editTitle)}>🖋</button>
+                </div>}
+
+
+                { editDescription ?  <div>
+                    <input value={descriptionInput} onChange={(e) => setDescriptionInput(e.target.value)}/>
+                    <button onClick={descriptionChange}>👍</button>
+                    <button onClick={descriptionCancel}>👎</button>
+                </div>: <div>
+                    <span>{descriptionInput}</span>
+                    <button onClick={() => setEditDescription(!editDescription)}>🖋</button>
+                </div>}
+
+                <select name="status" className="status" onChange={handleStatusChange}>
+                  <option value="backlog" selected={status==="backlog"}>Backlog</option>
+                  <option value="todo" selected={status==="todo"}>ToDo</option>
+                  <option value="in progress" selected={status==="in progress"}>In Progress</option>
+                  <option value="done" selected={status==="done"}>Done</option>
                 </select>
-                <select>
-                  <option value="high">High</option>
-                  <option value="mid">Mid</option>
-                  <option value="low">Low</option>
+                <select name="status" className="status" onChange={handlePriorityChange}>
+                  <option value="high" selected={priority==="high"}>High</option>
+                  <option value="mid" selected={priority==="mid"}>Mid</option>
+                  <option value="low" selected={priority==="low"}>Low</option>
                 </select>
 
                 <div>
-                    { editAssigne ?  <input value={assignee} /> : <span>{assignee}</span>}
-                    <button onClick={() => setEditAssigne(!editAssigne)}>
-                        { !editAssigne ? '🖋' : "📌"}
-                    </button>
+                    { editAssignee ?  <div>
+                        <input value={assigneeInput} onChange={(e) => setAssigneeInput(e.target.value)}/>
+                        <button onClick={assigneeChange}>👍</button>
+                        <button onClick={assigneeCancel}>👎</button>
+                    </div>: <div>
+                        <span>{assigneeInput}</span>
+                        <button onClick={() => setEditAssignee(!editAssignee)}>🖋</button>
+                    </div>}
                 </div>
             </div>
   );
